64b0003e94cebcd1225d05d79b756b38e91fd00e
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-11-27 20:41:45 -0300

    Corrigindo código da tabela Adicionando o <tbody> ao retor da <tr>

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -9,18 +9,20 @@ export function Transactions() {
             <SearchForm />
 
             <TransactionsTable>
-               <tr>
-                   <td>Desenvolvimento de site</td>
-                   <td className="income">R$ 12.000,00</td>
-                   <td>Serviço</td>
-                   <td>13/04/2022</td>
-               </tr>
-               <tr>
-                   <td>Almoço</td>
-                   <td className="outcome">R$ 40,00</td>
-                   <td>Alimentação</td>
-                   <td>13/04/2022</td>
-               </tr>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de site</td>
+                        <td className="income">R$ 12.000,00</td>
+                        <td>Serviço</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Almoço</td>
+                        <td className="outcome">R$ 40,00</td>
+                        <td>Alimentação</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                </tbody>
             </TransactionsTable>
         </TransactionsContainer>
     )
